619f56047f19e488afc5361ea9d862ffd977222c
Jason Wang <j10430001@gmail.com>
2018-11-27 12:13:51 +0800

    Jason Wang

--- src/com/tom/PokerGame.java
+++ src/com/tom/PokerGame.java
@@ -6,24 +6,39 @@ public class PokerGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Random random = new Random();
-		int number = random.nextInt(13) + 1;
-		System.out.print(number);
-		int color = random.nextInt(4);
-		switch(color) {
-		case 0:
-			System.out.print("S");
-			break;
-		case 1:
-			System.out.print("H");
-			break;
-		case 2:
-			System.out.print("D");
-			break;
-		case 3:
-			System.out.print("C");
-			break;
+		int[] suits = {0x2663, 0x2666, 0x2665, 0x2660};
+		int[] nums = new int[5];
+		nums[0] = 77;
+		nums[4] = 98;
+		System.out.println(nums[4]);
+		System.out.println(nums.length);
+		for(int i=0; i < nums.length; i++){
+			System.out.println(nums[i]);
 		}
+		char f = 0x2663;
+		System.out.println(f);
+				
+		
+/*		Random random = new Random();
+		for(int i=0; i<52; i++) {
+			int number = random.nextInt(13) + 1;
+			System.out.print(number);
+			int color = random.nextInt(4);
+			switch(color) {
+			case 0:
+				System.out.print("S");
+				break;
+			case 1:
+				System.out.print("H");
+				break;
+			case 2:
+				System.out.print("D");
+				break;
+			case 3:
+				System.out.print("C");
+				break;
+			}
+		}*/
 	}
 
 }
